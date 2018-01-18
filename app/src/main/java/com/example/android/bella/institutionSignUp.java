6c0ae17fb0407921816d7067503b1f1b2936a3d5
mostafa <mostafas.zaghlol@gmail.com>
2018-01-18 12:49:23 +0200

    first try to take image from galary

--- app/src/main/java/com/example/android/bella/institutionSignUp.java
+++ app/src/main/java/com/example/android/bella/institutionSignUp.java
@@ -1,12 +1,15 @@
 package com.example.android.bella;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class institutionSignUp extends AppCompatActivity {
-
+    protected Uri selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +27,28 @@ public class institutionSignUp extends AppCompatActivity {
             }
         });
     }
+
+    public void getImageFromGalary(View view) {
+        Intent pickerPhotoIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickerPhotoIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case 0:
+                if(requestCode == RESULT_OK) {
+                    Log.i("MainActivity", "case 0");
+                }
+                break;
+            case 1:
+                if(resultCode == RESULT_OK) {
+                    selectedImage = imageReturnedIntent.getData();
+                    Log.i("MainActivity", "selected Image = "+selectedImage);
+                }
+                break;
+        }
+    }
+}
 }
